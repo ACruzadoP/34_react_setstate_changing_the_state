282417e99ca1d7dbc5d6832c8ce2e9734fb6278f
Alberto <albertocruzadopleite@gmail.com>
2020-10-08 04:58:19 +0200

    Tomando snapshot de un ejercicio terminado

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function aumentar() {
+    console.log("It's working!")
+}
+function aumentar2() {
+    this.setState(
+        {count: 1}
+    )
 }
 
-export default App;
+class App extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            count: 0
+        }
+        aumentar2 = aumentar2.bind(this)
+        this.aumentar4 = this.aumentar4.bind(this)
+    }
+    
+    aumentar3() {
+        console.log("It's working!")
+    }
+
+    aumentar4() {
+        this.setState(prevState => {
+            return {
+                count: prevState.count + 1
+            }
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>{this.state.count}</h1>
+                <button onClick={aumentar}>Console log</button>
+                <button onClick={aumentar2}>Change! For real</button>
+                <button onClick={this.aumentar3}>Console log</button>
+                <button onClick={this.aumentar4}>Change! For real</button>
+            </div>
+        )
+    }
+}
+
+export default App
